e3e45ce19772c443bf95425a76ede5e98fcfb327
bdhwan <bdhwan@gmail.com>
2019-06-25 22:52:58 +0900

    fix version

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf node_modules
 rm -rf config/config.js
-docker build --no-cache --tag bdhwan/appflow-build-server:2.2 .
-docker push bdhwan/appflow-build-server:2.2
+docker build --no-cache --tag bdhwan/appflow-build-server:2.3 .
+docker push bdhwan/appflow-build-server:2.3
 #docker run -dti --mount type=bind,source=/Users/bdhwan/git/giftistar/appflow,target=/var/appflow bdhwan/appflow:0.3
